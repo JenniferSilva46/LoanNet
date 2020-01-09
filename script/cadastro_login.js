0f00b2c3e5fb957d3958f4c14cc2a8d344cb5a64
JenniferSilva46 <jennifer.silva@academico.ifpb.edu.br>
2020-01-08 22:04:53 -0300

    referenciando as páginas

---
+++ script/cadastro_login.js
@@ -0,0 +1,46 @@
+if(document.getElementById("bLogin") !== null){
+    document.getElementById("bLogin").addEventListener("click",
+    function che(){
+    var emailc= emailogin.value;
+    var senhac= senhalogin.value;
+    if(emailc==""){
+        alert("Email obrigatóorio!");
+    }
+    if(senhac==""){
+        alert("Senha obrigatória!");
+    }
+}
+
+    )
+}
+var cont=0;
+botao.onclick = function()
+{
+    var dig_nome= nome.value;
+    var dig_email= email.value;
+    var dig_senha= senha.value;
+    var dig_conf= conf_senha.value;
+    if(dig_nome==""){
+        alert("Nome obrigatorio!");
+        cont++;
+    }
+    if(dig_email==""){
+        alert("email obrigatorio!");
+        cont++;
+    }
+    if(dig_senha==""){
+        alert("senha obrigatorio!");
+        con++
+    }
+    if(dig_conf==""){
+        alert("Confirmar senha obrigatorio!");
+        cont++;
+    }
+    if(dig_conf!=dig_senha){
+        alert("Senha não compativel!");
+        cont++;
+    }
+    if(cont==0){
+        alert("Cadastro realizado com sucesso! \n\n Faça seu login!");        
+    }
+}
