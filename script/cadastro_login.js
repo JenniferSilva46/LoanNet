18933ef229842137868788b410b6719bb537c70f
JenniferSilva46 <jennifer.silva@academico.ifpb.edu.br>
2020-01-23 21:22:55 -0300

    Adicionando altenticação no login

--- script/cadastro_login.js
+++ script/cadastro_login.js
@@ -1,21 +1,45 @@
+//Login
+var emailc;
 if(document.getElementById("bLogin") !== null){
     document.getElementById("bLogin").addEventListener("click",
     function che(){
-    var emailc= emailogin.value;
+    emailc= emailogin.value;
     var senhac= senhalogin.value;
     if(emailc==""){
         alert("Email obrigatóorio!");
     }
     if(senhac==""){
         alert("Senha obrigatória!");
-    }
-}
+    }else{
+        //ARRAY PARA CADA USUARIO POSSIVELMENTE CADASTRADO
+    var busuario1=[localStorage.getItem('email1'),localStorage.getItem('senha1'),localStorage.getItem('nome1')]
+    var busuario2=[localStorage.getItem('email2'),localStorage.getItem('senha2'),localStorage.getItem('nome2')]
+    var busuario3=[localStorage.getItem('email3'),localStorage.getItem('senha3'),localStorage.getItem('nome3')]
+    var busuario4=[localStorage.getItem('email4'),localStorage.getItem('senha4'),localStorage.getItem('nome4')]
+//TENTANDO PEGAR O VALOR DO IPUT DO LOGIN E COMPARAR COM O CADASTRO
+    var email_login=document.getElementById("emailogin").value;
+    var senha_login=document.getElementById("senhalogin").value;
+    if(email_login==busuario1[0]&& senha_login==busuario1[1]){
+        alert("Seja bem vindo novamente"+ " "+ busuario1[2])
 
-    )
-}
+    }else if(email_login==busuario2[0]&& senha_login==busuario2[1]){
+        alert("Seja bem vindo, novamente"+ " " + busuario2[2])
 
+    }else if(email_login==busuario3[0]&& senha_login==busuario3[1]){
+        alert("Seja bem vindo, novamente"+ " " + busuario3[2])
+
+    }else if(email_login==busuario4[0]&& senha_login==busuario4[1]){
+        alert("Seja bem vindo, novamente"+ " " + busuario4[2])
+
+    }else{
+        alert("Essa conta não possui cadastro nesse site")
+    }}
+})
+}
+//Cadastro
 var cont=0;
-botao.onclick = function()
+if(document.getElementById("botao") !== null){
+    document.getElementById("botao").addEventListener("click", function()
 {
     var dig_nome= nome.value;
     var dig_email= email.value;
@@ -43,7 +67,8 @@ botao.onclick = function()
     }
     
     if(cont==0){
-        alert("Cadastro realizado com sucesso! \n\n Faça seu login!");        
+        alert("Cadastro realizado com sucesso! \n\n Faça seu login!");    
+        //CHAMANDO FUNÇÃO PARA CADA USUARIO    
             if(localStorage.qusuario==1){
                 user1();
                 localStorage.qusuario=Number(localStorage.qusuario)+1;
@@ -57,14 +82,16 @@ botao.onclick = function()
         localStorage.qusuario=1;
        }
     }
+})
 }
+//CADASTRANDO USUARIO
 function user1(){
     dig_nome=document.getElementById("nome").value;
     dig_email=document.getElementById("email").value;
     dig_senha=document.getElementById("senha").value;
-    localStorage.nome=dig_nome;
-    localStorage.email=dig_email;
-    localStorage.senha=dig_senha;
+    localStorage.nome1=dig_nome;
+    localStorage.email1=dig_email;
+    localStorage.senha1=dig_senha;
 }
 function user2(){
     let nome_user2=document.getElementById("nome").value;
